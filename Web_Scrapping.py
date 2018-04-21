55a0eafbcc196aa96e9a3d7c44fcb8e7e5c492c5
Dipak Tripathi <dipaktripathi10@gmail.com>
2018-04-22 01:58:32 +0530

    Final Commit

--- Web_Scrapping.py
+++ Web_Scrapping.py
@@ -10,36 +10,51 @@ all = soup.find_all("div", {"class":"propertyRow"})
 
 all[0].find("h4",{"class":"propPrice"}).text.replace("\n","")
 
+page_nr = soup.find_all("a",{"class":"Page"})[-1].text
+
+
 l=[]
-for item in all:
-    d={}
-    d["Address"] = item.find_all("span",{"class":"propAddressCollapse"})[0].text
-    d["Locality"] = item.find_all("span",{"class":"propAddressCollapse"})[1].text
-    d["Price"] = item.find("h4",{"class","propPrice"}).text.replace("\n","")
-    try:
-        d["Beds"] = item.find("span",{"class":"infoBed"}).find("b").text
-    except:
-        d["Beds"] = None
+for page in range(0, int(page_nr)*10,10):
+    r = requests.get("https://pythonhow.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/#t=0&s="+str(page)+".html")
+    c = r.content
+    soup = BeautifulSoup(c, "html.parser")
+    all = soup.find_all("div", {"class":"propertyRow"})
+    for item in all:
+        d={}
+        d["Address"] = item.find_all("span",{"class":"propAddressCollapse"})[0].text
+        try:
+            d["Locality"] = item.find_all("span",{"class":"propAddressCollapse"})[1].text
+        except:
+            d["Locality"] = None
+        d["Price"] = item.find("h4",{"class","propPrice"}).text.replace("\n","")
+        try:
+            d["Beds"] = item.find("span",{"class":"infoBed"}).find("b").text
+        except:
+            d["Beds"] = None
 
-    try:
-        d["Area"] = item.find("span",{"class":"infoSqFt"}).find("b").text
-    except:
-        d["Area"] = None
+        try:
+            d["Area"] = item.find("span",{"class":"infoSqFt"}).find("b").text
+        except:
+            d["Area"] = None
 
-    try:
-        d["Full Baths"] = item.find("span",{"class":"infoValueFullBath"}).find("b").text
-    except:
-        d["Full Baths"] = None
+        try:
+            d["Full Baths"] = item.find("span",{"class":"infoValueFullBath"}).find("b").text
+        except:
+            d["Full Baths"] = None
 
-    try:
-        d["Half Baths"] = item.find("span",{"class":"infoValueHalfBath"}).find("b").text
-    except:
-        d["Half Baths"] = None
-    for column_group in item.find_all("div",{"class":"columnGroup"}):
-        for feature_group, feature_name in zip(column_group.find_all("span",{"class":"featureGroup"}), column_group.find_all("span",{"class":"featureName"})):
-            if "Lot Size" in feature_group.text:
-                d["Lot Size"] = feature_name.text
-    l.append(d)
+        try:
+            d["Half Baths"] = item.find("span",{"class":"infoValueHalfBath"}).find("b").text
+        except:
+            d["Half Baths"] = None
+        for column_group in item.find_all("div",{"class":"columnGroup"}):
+            for feature_group, feature_name in zip(column_group.find_all("span",{"class":"featureGroup"}), column_group.find_all("span",{"class":"featureName"})):
+                if "Lot Size" in feature_group.text:
+                    d["Lot Size"] = feature_name.text
+        l.append(d)
+
+
+
+
 
 import pandas
 
