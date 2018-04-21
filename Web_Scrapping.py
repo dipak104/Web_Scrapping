42cd12aba50c87e3650f95ce044660123acad43c
Dipak Tripathi <dipaktripathi10@gmail.com>
2018-04-21 19:03:33 +0530

    Complete Scrapping Code

---
+++ Web_Scrapping.py
@@ -0,0 +1,48 @@
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get("https://pythonhow.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/")
+c = r.content
+
+soup = BeautifulSoup(c, "html.parser")
+
+all = soup.find_all("div", {"class":"propertyRow"})
+
+all[0].find("h4",{"class":"propPrice"}).text.replace("\n","")
+
+l=[]
+for item in all:
+    d={}
+    d["Address"] = item.find_all("span",{"class":"propAddressCollapse"})[0].text
+    d["Locality"] = item.find_all("span",{"class":"propAddressCollapse"})[1].text
+    d["Price"] = item.find("h4",{"class","propPrice"}).text.replace("\n","")
+    try:
+        d["Beds"] = item.find("span",{"class":"infoBed"}).find("b").text
+    except:
+        d["Beds"] = None
+
+    try:
+        d["Area"] = item.find("span",{"class":"infoSqFt"}).find("b").text
+    except:
+        d["Area"] = None
+
+    try:
+        d["Full Baths"] = item.find("span",{"class":"infoValueFullBath"}).find("b").text
+    except:
+        d["Full Baths"] = None
+
+    try:
+        d["Half Baths"] = item.find("span",{"class":"infoValueHalfBath"}).find("b").text
+    except:
+        d["Half Baths"] = None
+    for column_group in item.find_all("div",{"class":"columnGroup"}):
+        for feature_group, feature_name in zip(column_group.find_all("span",{"class":"featureGroup"}), column_group.find_all("span",{"class":"featureName"})):
+            if "Lot Size" in feature_group.text:
+                d["Lot Size"] = feature_name.text
+    l.append(d)
+
+import pandas
+
+df = pandas.DataFrame(l)
+
+df.to_csv("output.csv")
